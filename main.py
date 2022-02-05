889189a7b01d6a0ed38f87f83a0b69b6b321e0f3
pavloteyfel <pavloteyfel@gmail.com>
2022-02-05 21:41:40 +0100

    feature: Add sqlalchemy models

--- main.py
+++ main.py
@@ -9,27 +9,4 @@ app = FastAPI()
 def read_root():
     return "The server is running."
 
-@dataclass
-class Customer:
-    id: int
-    first_name: str
-    last_name: str
-    email_address: str
 
-
-@dataclass
-class Room:
-    id: int
-    number: str
-    size: int
-    price: int
-
-
-@dataclass
-class Booking:
-    id: int
-    from_date: date
-    to_date: date
-    customer: Customer
-    room: Room
-    price: int
