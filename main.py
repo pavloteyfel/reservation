229816e2f2e8f7ab18d4751ea25ec4166232f680
pavloteyfel <pavloteyfel@gmail.com>
2022-02-05 21:58:21 +0100

    feature: Connect db

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from dataclasses import dataclass
-from datetime import date
 from fastapi import FastAPI
+
+from hotel.db.engine import init_db, DBSession
+from hotel.db.models import DBRoom
 
 
 app = FastAPI()
+
+DB_FILE = "sqlite:///hotel.db"
+
+
+@app.on_event("startup")
+def startup_event():
+    init_db(DB_FILE)
+
 
 @app.get("/")
 def read_root():
     return "The server is running."
 
-
+@app.get("/rooms")
+def real_all_rooms():
+    session = DBSession()
+    rooms = session.query(DBRoom).all()
+    return rooms
