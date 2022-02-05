b5e9ef7830c30acd2d6efd96e17798b28b372aca
pavloteyfel <pavloteyfel@gmail.com>
2022-02-05 21:23:18 +0100

    feature: Add fastapi

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
 from datetime import date
+from fastapi import FastAPI
 
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return "The server is running."
 
 @dataclass
 class Customer:
