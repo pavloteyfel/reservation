4021c043268e840f523b70b08b3271892f105bc0
pavloteyfel <pavloteyfel@gmail.com>
2022-02-08 22:04:05 +0100

    refactor: Change data object

--- hotel/operations/rooms.py
+++ hotel/operations/rooms.py
@@ -1,11 +1,12 @@
-from hotel.db.models import DBRoom, to_dict
 from hotel.db.engine import DBSession
+from hotel.db.models import DBRoom, to_dict
 
 
 def read_all_rooms():
     session = DBSession()
     rooms = session.query(DBRoom).all()
     return [to_dict(room) for room in rooms]
+
 
 def read_room(room_id: int):
     session = DBSession()
