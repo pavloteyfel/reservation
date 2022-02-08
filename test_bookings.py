bedbfff0aef455a2e0bfcb31184b98354337a630
pavloteyfel <pavloteyfel@gmail.com>
2022-02-08 22:02:01 +0100

    refactor: Change data object to dictStrAny

--- test_bookings.py
+++ test_bookings.py
@@ -1,35 +1,33 @@
 import unittest
-from hotel.operations.bookings import BookingCreateData, DataObject, InvalidDateError, create_booking
+
+from hotel.operations.bookings import BookingCreateData, DictStrAny, \
+    InvalidDateError, create_booking
 
 
 class DataInterfaceStub:
-    def read_by_id(self, id: int) -> DataObject:
+    def read_by_id(self, id: int) -> DictStrAny:
         raise NotImplementedError()
 
-    def read_all(self) -> list[DataObject]:
+    def read_all(self) -> list[DictStrAny]:
         raise NotImplementedError()
-        ...
 
-    def create(self, data: DataObject) -> DataObject:
+    def create(self, data: DictStrAny) -> DictStrAny:
         raise NotImplementedError()
-        ...
 
-    def update(self, id: int, data: DataObject) -> DataObject:
+    def update(self, id: int, data: DictStrAny) -> DictStrAny:
         raise NotImplementedError()
-        ...
 
-    def delete(self, id: int) -> DataObject:
+    def delete(self, id: int) -> DictStrAny:
         raise NotImplementedError()
-        ...
 
 
 class RoomInterface(DataInterfaceStub):
-    def read_by_id(self, id: int) -> DataObject:
+    def read_by_id(self, id: int) -> DictStrAny:
         return {"id": id, "number": "101", "size": 10, "price": 150_00}
 
 
 class BookingInterface(DataInterfaceStub):
-    def create(self, data: DataObject) -> DataObject:
+    def create(self, data: DictStrAny) -> DictStrAny:
         booking = dict(data)
         booking["id"] = 1
         return booking
