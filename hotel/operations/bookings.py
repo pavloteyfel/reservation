3a3e720a0a2f2131f6c3a37ac56a430c389d6ea3
pavloteyfel <pavloteyfel@gmail.com>
2022-02-08 22:02:31 +0100

    refactor: Change data object

--- hotel/operations/bookings.py
+++ hotel/operations/bookings.py
@@ -1,8 +1,9 @@
-from pydantic import BaseModel
-from typing import Optional
 from datetime import date
+from typing import Optional
 
-from hotel.operations.interface import DataInterface, DataObject
+from pydantic import BaseModel
+
+from hotel.operations.interface import DataInterface, DictStrAny
 
 
 class InvalidDateError(Exception):
@@ -22,19 +23,17 @@ class CustomerUpdateData(BaseModel):
     email_address: Optional[str]
 
 
-def read_all_bookings(booking_interface: DataInterface) -> DataInterface:
+def read_all_bookings(booking_interface: DataInterface) -> list[DictStrAny]:
     return booking_interface.read_all()
 
 
-def read_booking(booking_id: int, booking_interface: DataInterface) -> DataObject:
+def read_booking(booking_id: int,
+                 booking_interface: DataInterface) -> DictStrAny:
     return booking_interface.read_by_id(booking_id)
 
 
-def create_booking(
-    data: BookingCreateData,
-    booking_interface: DataInterface,
-    room_interface: DataInterface,
-) -> DataObject:
+def create_booking(data: BookingCreateData, booking_interface: DataInterface,
+                   room_interface: DataInterface) -> DictStrAny:
     room = room_interface.read_by_id(data.room_id)
     days = (data.to_date - data.from_date).days
     if days <= 0:
@@ -44,5 +43,6 @@ def create_booking(
     return booking_interface.create(booking_dict)
 
 
-def delete_booking(booking_id: int, booking_interface: DataInterface) -> DataObject:
+def delete_booking(booking_id: int,
+                   booking_interface: DataInterface) -> DictStrAny:
     return booking_interface.delete(booking_id)
